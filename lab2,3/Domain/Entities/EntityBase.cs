89836c83b3adaacabbfb449cc653ccb31429c511
shatovisk <57827686+shatovisk@users.noreply.github.com>
2020-11-05 16:40:58 +0300

    Add files via upload

---
+++ lab2,3/Domain/Entities/EntityBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Domain.Entities
+{
+    public abstract class EntityBase
+    {
+        protected EntityBase() => DateAdded = DateTime.UtcNow;
+
+        [Required]
+        public Guid Id { get;set; }
+
+        [Display(Name = "Вопрос")]
+        public virtual string Title { get; set; }
+
+        [Display(Name = "Ответ")]
+        public virtual string Text { get; set; }
+
+        [Display(Name = "Титульная картинка")]
+        public virtual string TitleImagePath { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime DateAdded { get; set; }
+    }
+}
---
+++ lab2,3/Domain/Entities/ServiceItem.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Domain.Entities
+{
+    public class ServiceItem : EntityBase
+    {
+        [Required(ErrorMessage = "Введите вопрос")]
+        [Display(Name = "Вопрос")]
+        public override string Title { get; set; }
+
+        [Display(Name = "Ответ")]
+        public override string Text { get; set; }
+
+    }
+}
